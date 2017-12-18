8bd03261ea480f1d5f7ac68d763a82b9645663b8
HANATANI Takuma <mail@potato4d.me>
2017-12-19 02:04:35 +0900

    Add Vue version

---
+++ parcel-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Parcel for Vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
