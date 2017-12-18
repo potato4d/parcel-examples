5dfada26bf0e466e8dbe8658c2c1147ebc1ce30c
HANATANI Takuma <mail@potato4d.me>
2017-12-19 02:16:29 +0900

    Add React version

---
+++ parcel-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class App extends Component {
+  render () {
+    return (
+      <h1>
+        Parcel for React.
+      </h1>
+    )
+  }
+}
+export default App
