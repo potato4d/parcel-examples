959051e0d95b1a8a3660be846e14e9e6b6d80c63
HANATANI Takuma <mail@potato4d.me>
2017-12-19 03:27:44 +0900

    Add preact version

---
+++ parcel-preact/src/App.js
@@ -0,0 +1,12 @@
+import { render, Component } from 'preact'
+
+class App extends Component {
+  render () {
+    return (
+      <h1>
+        Parcel for preact.
+      </h1>
+    )
+  }
+}
+export default App
